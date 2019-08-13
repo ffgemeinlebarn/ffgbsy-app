7104ecf7f782f163b181639193f11a96cfdec75c
Jakob Vesely <jakob@appartig.at>
2019-08-13 13:58:28 +0200

    Status Bar Farbe anpassen

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -35,6 +35,7 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString('#ffffff');
       this.splashScreen.hide();
     });
   }
