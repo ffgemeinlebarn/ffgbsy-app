1463a53bf35a95bb128edca50433a143fdde2819
Jakob <jakob@appartig.at>
2022-05-16 20:39:46 +0200

    Init PWA (#2)
    * ng add @angular/pwa
    
    * Add Deployment
    
    * First Empty Page
    
    * Dont Deploy on Feature
    
    * Trigger on Opend Pull Request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'folder/Inbox',
+        redirectTo: 'init',
         pathMatch: 'full'
     },
     {
-        path: 'folder/:id',
-        loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
+        path: 'init',
+        loadChildren: () => import('./start/init/init.module').then(m => m.InitPageModule)
     }
 ];
 
--- src/app/start/init/init.page.spec.ts
+++ src/app/start/init/init.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { FolderPage } from './folder.page';
 
-describe('FolderPage', () => {
-    let component: FolderPage;
-    let fixture: ComponentFixture<FolderPage>;
+import { InitPage } from './init.page';
+
+describe('InitPage', () => {
+    let component: InitPage;
+    let fixture: ComponentFixture<InitPage>;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            declarations: [FolderPage],
-            imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+            declarations: [InitPage],
+            imports: [IonicModule.forRoot()]
         }).compileComponents();
 
-        fixture = TestBed.createComponent(FolderPage);
+        fixture = TestBed.createComponent(InitPage);
         component = fixture.componentInstance;
         fixture.detectChanges();
     }));
