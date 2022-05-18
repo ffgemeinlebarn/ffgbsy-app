01975d9052318c3b933666ddbfdf6940d729da9c
Jakob <jakob@appartig.at>
2022-05-18 18:39:54 +0200

    API (#6)

--- src/app/pages/start/init/init.page.ts
+++ src/app/pages/start/init/init.page.ts
@@ -16,124 +16,124 @@ import { HttpClient } from '@angular/common/http';
 import { BestellungenHandlerService } from 'src/app/services/bestellungen/bestellungen-handler.service';
 
 @Component({
-  selector: 'app-init',
-  templateUrl: './init.page.html',
-  styleUrls: ['./init.page.scss'],
+    selector: 'app-init',
+    templateUrl: './init.page.html',
+    styleUrls: ['./init.page.scss'],
 })
 export class InitPage implements OnInit {
 
-  public messageLineOne: string = "";
-  public messageLineTwo: string = "";
-  public datenSynchronisiert: boolean = false;
-  public datenSynchronisierungLaeuft: boolean = false;
-  public lokalesGeraetReady: boolean = false;
+    public messageLineOne: string = "";
+    public messageLineTwo: string = "";
+    public datenSynchronisiert: boolean = false;
+    public datenSynchronisierungLaeuft: boolean = false;
+    public lokalesGeraetReady: boolean = false;
 
-  public geraet: any = null;
-  public geraet_message: string = '';
+    public geraet: any = null;
+    public geraet_message: string = '';
 
-  public systemstatus: any = [];
+    public systemstatus: any = [];
 
-  constructor(
-    private http: HttpClient,
-    public actionSheetController: ActionSheetController,
-    public bestellungsHandler: BestellungenHandlerService,
-    public data: DataService,
-    public session: SessionService,
-    public settings: SettingsService,
-    public frontend: FrontendService
-  ) {
-    this.messageLineOne = "Warte auf Synchronisierung der Daten.";
-    this.messageLineTwo = "";
-  }
-
-  ngOnInit() {}
-
-  datenSynchronisierungStarten(){
-
-    this.datenSynchronisierungLaeuft = true;
-    this.messageLineOne = "Starte Synchronisierung ...";
-    this.messageLineTwo = "";
-
-    this.data.download().then(result => {
-
-      this.refrehLocalGeraet();
-
-      if (result){
-        this.datenSynchronisiert = true;
-        this.messageLineOne = "Daten erfolgreich synchronisiert!";
-        if (!this.geraet){
-          this.messageLineTwo = "Bevor du deine Session starten kannst, muss das lokale Gerät konfiguriert werden.";
-        }else{
-          this.messageLineTwo = "Du kannst jetzt deine Session starten.";
-        }
-      }else{
-        this.datenSynchronisiert = false;
-        this.messageLineOne = "Es trat ein Fehler auf!";
-        this.messageLineTwo = "Daten wurden nicht synchronisiert!";
-      }
-
-      this.datenSynchronisierungLaeuft = false;
-
-    });
-  }
-
-  refrehLocalGeraet(){
-    
-    this.geraet = this.data.getGeraetById(this.settings.locale.diesesGeraetId);
-
-    if (this.geraet){
-      this.lokalesGeraetReady = true;
-      this.geraet_message = 'Lokale Gerät-Konfiguration: ' + this.geraet.hersteller + ' ' + this.geraet.type + ' (' + this.geraet.ip + ')';
-    }else{
-      this.geraet_message = 'Dieses lokale Gerät ist noch keinem Gerät im System zugewiesen!';
+    constructor(
+        private http: HttpClient,
+        public actionSheetController: ActionSheetController,
+        public bestellungsHandler: BestellungenHandlerService,
+        public data: DataService,
+        public session: SessionService,
+        public settings: SettingsService,
+        public frontend: FrontendService
+    ) {
+        this.messageLineOne = "Warte auf Synchronisierung der Daten.";
+        this.messageLineTwo = "";
     }
 
-  }
+    ngOnInit() { }
 
-  systemstatusAbrufen(){
+    datenSynchronisierungStarten() {
 
-    let startingDateTime = new Date();
-      this.frontend.showLoadingSpinner(null, 'Prüfe Systemstatus');
-      this.http.get<any>(this.settings.api.url + '/systemstatus').subscribe(systemstatus => {
-        this.frontend.hideLoadingSpinner();
-        this.systemstatus = [
-          {
-            text: 'API',
-            icon: systemstatus.api.connected ? 'checkmark-circle' : 'alert',
-            cssClass: systemstatus.api.connected ? 'systemstatus-success' : 'systemstatus-danger',
-            handler: () => { return false; }
-          }
-        ]
+        this.datenSynchronisierungLaeuft = true;
+        this.messageLineOne = "Starte Synchronisierung ...";
+        this.messageLineTwo = "";
 
-        for(let i=0;i<systemstatus.drucker.length;i++){
+        this.data.download().then(result => {
 
-          this.systemstatus.push({
-            text: 'Drucker ' + systemstatus.drucker[i].name,
-            icon: systemstatus.drucker[i].connected ? 'checkmark-circle' : 'alert',
-            cssClass: systemstatus.drucker[i].connected ? 'systemstatus-success' : 'systemstatus-danger',
-            handler: () => { return false; }
-          });
+            //   this.refrehLocalGeraet();
+
+            if (result) {
+                this.datenSynchronisiert = true;
+                this.messageLineOne = "Daten erfolgreich synchronisiert!";
+                if (!this.geraet) {
+                    this.messageLineTwo = "Bevor du deine Session starten kannst, muss das lokale Gerät konfiguriert werden.";
+                } else {
+                    this.messageLineTwo = "Du kannst jetzt deine Session starten.";
+                }
+            } else {
+                this.datenSynchronisiert = false;
+                this.messageLineOne = "Es trat ein Fehler auf!";
+                this.messageLineTwo = "Daten wurden nicht synchronisiert!";
+            }
+
+            this.datenSynchronisierungLaeuft = false;
+
+        });
+    }
+
+    refrehLocalGeraet() {
+
+        this.geraet = this.data.getGeraetById(this.settings.locale.diesesGeraetId);
+
+        if (this.geraet) {
+            this.lokalesGeraetReady = true;
+            this.geraet_message = 'Lokale Gerät-Konfiguration: ' + this.geraet.hersteller + ' ' + this.geraet.type + ' (' + this.geraet.ip + ')';
+        } else {
+            this.geraet_message = 'Dieses lokale Gerät ist noch keinem Gerät im System zugewiesen!';
         }
 
-        this.showSystemstatus();
-      },
-      err => {
-        this.frontend.hideLoadingSpinner();
-        console.log("Error occured: ", err);
-      });
-  }
+    }
 
-  async showSystemstatus() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Systemstatus Kurzbericht',
-      buttons: this.systemstatus
-    });
-    await actionSheet.present();
-  }
+    systemstatusAbrufen() {
 
-  @ViewChild(IonSelect, {static: false}) select: IonSelect;
-  openGeraeteSelect(){
-    this.select.open();
-  }
+        let startingDateTime = new Date();
+        this.frontend.showLoadingSpinner(null, 'Prüfe Systemstatus');
+        this.http.get<any>(this.settings.api.url + '/systemstatus').subscribe(systemstatus => {
+            this.frontend.hideLoadingSpinner();
+            this.systemstatus = [
+                {
+                    text: 'API',
+                    icon: systemstatus.api.connected ? 'checkmark-circle' : 'alert',
+                    cssClass: systemstatus.api.connected ? 'systemstatus-success' : 'systemstatus-danger',
+                    handler: () => { return false; }
+                }
+            ]
+
+            for (let i = 0; i < systemstatus.drucker.length; i++) {
+
+                this.systemstatus.push({
+                    text: 'Drucker ' + systemstatus.drucker[i].name,
+                    icon: systemstatus.drucker[i].connected ? 'checkmark-circle' : 'alert',
+                    cssClass: systemstatus.drucker[i].connected ? 'systemstatus-success' : 'systemstatus-danger',
+                    handler: () => { return false; }
+                });
+            }
+
+            this.showSystemstatus();
+        },
+            err => {
+                this.frontend.hideLoadingSpinner();
+                console.log("Error occured: ", err);
+            });
+    }
+
+    async showSystemstatus() {
+        const actionSheet = await this.actionSheetController.create({
+            header: 'Systemstatus Kurzbericht',
+            buttons: this.systemstatus
+        });
+        await actionSheet.present();
+    }
+
+    @ViewChild(IonSelect, { static: false }) select: IonSelect;
+    openGeraeteSelect() {
+        this.select.open();
+    }
 
 }
