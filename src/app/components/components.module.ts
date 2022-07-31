1cf646237886aaa0254d1465647b38dceffc076f
Jakob Vesely <dev@jakob.fyi>
2022-07-31 19:21:59 +0200

    fix build

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InitRequirementComponent } from './init-requirement/init-requirement.component';
-
-
 
 @NgModule({
     declarations: [
-        InitRequirementComponent
     ],
     imports: [
         CommonModule
     ],
     exports: [
-        InitRequirementComponent
     ]
 })
 export class ComponentsModule { }
--- src/app/pages/start/init/init.module.ts
+++ src/app/pages/start/init/init.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { InitPage } from './init.page';
-import { InitRequirementComponent } from 'src/app/components/init-requirement/init-requirement.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 
 const routes: Routes = [
