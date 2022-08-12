39da7efbad87f04db52dd5aae0b2a642dc8cc123
Jakob <jakob@appartig.at>
2022-08-12 19:02:19 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    api: 'https://internal.ffgbsy.ff-gemeinlebarn.at',
+    api: 'https://api.ffgbsy.ff-gemeinlebarn.at',
     localAdminPin: '17108'
 };
