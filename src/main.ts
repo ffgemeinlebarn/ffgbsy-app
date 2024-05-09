69feb3f91c77f72384e7c003b30df5e5db3a3681
Jakob Vesely <engineering@jakob.fyi>
2024-05-09 20:29:27 +0200

    a

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
