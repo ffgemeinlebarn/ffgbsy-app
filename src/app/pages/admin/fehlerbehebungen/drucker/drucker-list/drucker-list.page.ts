f40690844c0e10fe01748e6ed5568640714431c8
Jakob Vesely <engineering@jakob.fyi>
2024-07-30 23:55:38 +0200

    Drucker Bearbeiten

--- src/app/pages/admin/fehlerbehebungen/drucker/drucker-list/drucker-list.page.ts
+++ src/app/pages/admin/fehlerbehebungen/drucker/drucker-list/drucker-list.page.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { IonContent, IonHeader, IonList, IonMenuButton, IonTitle, IonToolbar, ViewDidEnter } from '@ionic/angular/standalone';
+import { Drucker } from 'src/app/classes/drucker.class';
+import { PageSpinnerComponent } from 'src/app/components/page-spinner/page-spinner.component';
+import { DruckerService } from 'src/app/services/drucker/drucker.service';
 
 @Component({
-  selector: 'ffgbsy-drucker-list',
-  templateUrl: './drucker-list.page.html',
-  styleUrls: ['./drucker-list.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+    selector: 'ffgbsy-drucker-list',
+    templateUrl: './drucker-list.page.html',
+    styleUrls: ['./drucker-list.page.scss'],
+    standalone: true,
+    imports: [IonList, IonContent, IonHeader, RouterLink, IonTitle, IonToolbar, IonMenuButton, PageSpinnerComponent, CommonModule, FormsModule]
 })
-export class DruckerListPage implements OnInit {
+export class DruckerListPage implements ViewDidEnter {
+    private druckerService = inject(DruckerService);
 
-  constructor() { }
+    public drucker = signal<Drucker[]>(null);
 
-  ngOnInit() {
-  }
+    ionViewDidEnter(): void {
+        this.druckerService.readAll().subscribe(items => this.drucker.set(items));
+    }
 
 }
